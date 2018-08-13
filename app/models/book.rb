5b96399b9e74dbdbb9d7596a7b0c4a5e3f8435e3
azhar nahoor <azhar@Thinkpad.scmauritius.com>
2018-08-13 10:28:44 +0400

    .git

---
+++ app/models/book.rb
@@ -0,0 +1,16 @@
+class Book < ApplicationRecord
+    has_many :comments
+    has_many :book_categories
+    mount_uploader :image, ImageUploader
+    validates :image, presence: true
+
+    # filters on 'country_id' foreign key
+    scope :with_title, lambda { |titles|
+    where(title: [*titles])
+    }
+
+    ActiveAdmin.register Book do
+        filter :title
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
+  resources :book_categories
+  resources :categories
+  resources :comments
+  
+  resources :books do
+    resources :comments
+    get :book_by_category
+  end
+
+  root 'books#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
